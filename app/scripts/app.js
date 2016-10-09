4fc9d31dde49db3078a4e02f119f2968332cb5c5
Igor Gorlov <gorlov35@gmail.com>
2016-10-09 23:22:47 +0300

    add catalog, popup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,13 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import 'owl-carousel-2-beta';
+import 'magnific-popup';
+import 'jquery-ui/ui/core';
+import 'jquery-ui/ui/widget';
+import 'jquery-ui/ui/widgets/button';
+import 'jquery-ui/ui/widgets/spinner';
+import 'jquery-validation';
+
 
 $(() => {
 	svg4everybody();
@@ -19,6 +26,91 @@ $(() => {
 
 	});
 
+
+	$('.popup-trigger').magnificPopup({
+		type: 'inline'
+	});
+
+
+	// $(".input__field").each(function() {
+	// 	if ($(this).val() != "") {
+	// 		$(this).parent().addClass("animation");
+	// 	}
+	// });
+
+	$(".input__field").focus(function(){
+		$(this).parent().addClass("animation");
+	});
+
+	//Remove animation(s) when input is no longer focused
+	$(".input__field").focusout(function(){
+		if($(this).val() === "")
+			$(this).parent().removeClass("animation");
+		// $(this).parent().removeClass("animation-color");
+	});
+
+	const amountSpinner = $('.spinner__field');
+	const amountSpinnerUp = $('.spinner__button_up');
+	const amountSpinnerDown = $('.spinner__button_down');
+
+	amountSpinner.spinner({
+		max: 50,
+		min: 0,
+		step: 1,
+		decimals: 0
+	});
+
+	amountSpinnerUp.click(function () {
+		amountSpinner.spinner('stepUp');
+	});
+
+	amountSpinnerDown.click(function () {
+		amountSpinner.spinner('stepDown');
+	});
+
+
+	// validation
+
+	var popupForm = $('#popup-form');
+
+	popupForm.validate({
+		rules: {
+			name: {
+				required: true
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			city: {
+				required: true
+			},
+			product: {
+				required: true
+			},
+			note: {
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: ''
+			},
+			email: {
+				required: '',
+				email: ''
+			},
+			city: {
+				required: ''
+			},
+			product: {
+				required: ''
+			},
+			note: {
+				required: ''
+			}
+		}
+	});
 
 
 });
